897264ffe6ee19fd005c44459a68150c34dbd63a
aa515265 <477617847@qq.com>
2018-02-10 15:03:57 +0800

    Update Gitmain.java

--- src/com/git/Gitmain.java
+++ src/com/git/Gitmain.java
@@ -5,7 +5,7 @@ public class Gitmain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int aaaaa = 2222;
-		System.out.println(aaaaa);
+		System.out.println(aaaaa+5555);
 	}
 
 }
